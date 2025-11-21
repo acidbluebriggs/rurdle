48739787ae060db37f8fe80e6fd5933da0b6ba38
Briggs <acidbriggs@gmail.com>
2025-11-21 10:01:56 -0500

    Adding clap for command line processing debug option has been added

--- src/bin/rurdle.rs
+++ src/bin/rurdle.rs
@@ -1,19 +1,29 @@
+use clap::Parser;
+use rurdle::grid::Dictionary;
 use rurdle::{WORDS, run};
 use std::collections::BTreeSet;
-use std::env;
-use rurdle::grid::Dictionary;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    debug: Option<String>,
+    #[arg(long)]
+    hard: bool,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    // let is_hard_mode = args.contains(&"hard".to_string());
+    let args: Args = Args::parse();
     let word_set: BTreeSet<String> = WORDS.lines().map(|w| w.to_ascii_uppercase()).collect();
     let dictionary = Dictionary::new(word_set);
+    let random_word = fetch_word(args, &dictionary);
+    run(random_word, &dictionary);
+}
 
-    let random_word = if args.contains(&"debug".to_string()) {
-        "RENEW".to_string()
+fn fetch_word(args: Args, dictionary: &Dictionary) -> String {
+    if args.debug.is_some() {
+        args.debug.unwrap().to_ascii_uppercase().to_string()
     } else {
         dictionary.random_word()
-    };
-
-    run(random_word.to_string(), &dictionary);
+    }
 }
